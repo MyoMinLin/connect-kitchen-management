f8a00d7a86fc401c3200b198e904a5815f88a178
Myo Min Lin <MyoMinLin@users.noreply.github.com>
2025-10-12 00:10:14 +0900

    updated to make API URL configurable

--- client/src/utils/apiConfig.ts
+++ client/src/utils/apiConfig.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL =  'http://localhost:4000'; //'https://connect-kitchen-management-backend.vercel.app';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:4000';
